f0da56de329c702fb502bfb80e7b136b62a22055
Charlesss7 <90251876+Charlesss7@users.noreply.github.com>
2021-09-29 22:59:49 +0700

    Changes

---
+++ Assets/Scripts/Managers/EnemyManager.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+public class EnemyManager : MonoBehaviour
+{
+    public PlayerHealth playerHealth;
+    public float spawnTime = 3f;
+    public Transform[] spawnPoints;
+
+    [SerializeField]
+    MonoBehaviour factory;
+    IFactory Factory { get { return factory as IFactory; } }
+
+    void Start()
+    {
+        //count = transform.childCount;
+        //spawnPoints = new Transform[count];
+        //for(int i = 0; i < count; i++)
+        //{
+        //    spawnPoints[i] = transform.GetChild(i);
+        //}
+        //Mengeksekusi fungs Spawn setiap beberapa detik sesui dengan nilai spawnTime
+        InvokeRepeating("Spawn", spawnTime, spawnTime);
+    }
+
+    void Spawn()
+    {
+        //Jika player telah mati maka tidak membuat enemy baru
+        if (playerHealth.currentHealth <= 0f)
+        {
+            return;
+        }
+
+        //Mendapatkan nilai random
+        int spawnPointIndex = Random.Range(0, spawnPoints.Length);
+        int spawnEnemy = Random.Range(0, 3);
+        //Memduplikasi enemy
+        Factory.FactoryMethod(spawnEnemy);
+    }
+}
---
+++ Assets/Scripts/Player/PlayerHealth.cs
@@ -0,0 +1,94 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public int startingHealth = 100;
+    public int currentHealth;
+    public Slider healthSlider;
+    public Image damageImage;
+    public AudioClip deathClip;
+    public float flashSpeed = 5f;
+    public Color flashColour = new Color(1f, 0f, 0f, 0.1f);
+
+
+    Animator anim;
+    AudioSource playerAudio;
+    PlayerMovement playerMovement;
+    PlayerShooting playerShooting;
+    bool isDead;
+    bool damaged;
+
+
+    void Awake()
+    {
+        //Mendapatkan refernce komponen
+        anim = GetComponent<Animator>();
+        playerAudio = GetComponent<AudioSource>();
+        playerMovement = GetComponent<PlayerMovement>();
+        playerShooting = GetComponentInChildren<PlayerShooting>();
+
+        currentHealth = startingHealth;
+    }
+
+
+    void Update()
+    {
+        //jika terkena dmg
+        if (damaged)
+        {
+            //Merubah warna gambar menjadi value dari flashColour
+            damageImage.color = flashColour;
+        }
+        else
+        {
+            //Fade out damage image
+            damageImage.color = Color.Lerp(damageImage.color, Color.clear, flashSpeed * Time.deltaTime);
+        }
+        //set dmg false
+        damaged = false;
+    }
+
+    //func dpt dmg
+    public void TakeDamage(int amount)
+    {
+        damaged = true;
+        //-- health
+        currentHealth -= amount;
+        //ubah tampilan health slider'
+        healthSlider.value = currentHealth;
+        //suara jika terdmg
+        playerAudio.Play();
+        //Memanggil method Death() jika darahnya kurang dari sama dengan 10 dan belu mati
+        if (currentHealth <= 0 && !isDead)
+        {
+            Death();
+        }
+    }
+
+
+    void Death()
+    {
+        isDead = true;
+
+        playerShooting.DisableEffects();
+
+        //trigger animasi die
+        anim.SetTrigger("Die");
+
+        //suara ktk amti
+        playerAudio.clip = deathClip;
+        playerAudio.Play();
+
+        //matikan script playermovement
+        playerMovement.enabled = false;
+        playerShooting.enabled = false;
+    }
+    public void RestartLevel()
+    {
+        //meload ulang scene dengan index 0 pada build setting
+        SceneManager.LoadScene(0);
+    }
+}
